c03fe14507cadf73248c8ff0c4e8a743636385e4
José R. Gutierrez <josefo727@gmail.com>
2025-12-24 22:48:18 -0500

    feat: add FilamentUser implementation to User model
    - Implement `FilamentUser` contract in `User` model
    - Add `canAccessPanel` method returning true for panel access

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,11 +5,13 @@ declare(strict_types=1);
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Filament\Models\Contracts\FilamentUser;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Filament\Panel;
 
-final class User extends Authenticatable
+final class User extends Authenticatable implements FilamentUser
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory;
@@ -48,5 +50,10 @@ final class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return true;
     }
 }
